8bf51fc300bf49bebe61bb4a8f4dca25032cac48
Ankit Tiwari <at7666.at@gmail.com>
2019-12-23 17:20:41 +0530

    new code

--- coding-challenges/week10/Day01/index.js
+++ coding-challenges/week10/Day01/index.js
@@ -1,16 +1,15 @@
-function AtmPin(pin) {
-    var Chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-
-    for (i = 0; i < pin.length; i++) {
-        if (Chars.includes(pin[i]) === false) {
+function atmPin(pin) {
+    var len = pin.length;
+    if (len != 4 && len != 6) {
+        return false;
+    }
+    for (var char of pin) {
+        if (isNaN(char)) {
             return false;
         }
     }
-    if (pin.length === 4 | pin.length === 6) {
-        return true;
-    }
-    return false;
+    return true;
 }
-console.log(AtmPin("1345"))
-console.log(AtmPin("134145"))
-console.log(AtmPin("34145"))
+console.log(atmPin("1345"))
+console.log(atmPin("134145"))
+console.log(atmPin("34145"))
